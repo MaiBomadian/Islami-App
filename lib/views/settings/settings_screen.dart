b51597665ab52d90d174ca4c6600291c9fba0303
Mai <MaiBomadian94>
2024-02-10 01:39:12 +0200

    localization

--- lib/views/settings/settings_screen.dart
+++ lib/views/settings/settings_screen.dart
@@ -1,17 +1,53 @@
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
+  SettingsScreen({super.key});
+
+  final List<String> languages = [
+    'English',
+    'عربي',
+  ];
+  final List<String> themeLits = [
+    'Light',
+    'Dark',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Settings',
-        style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'El Messiri'),
+    var theme = Theme.of(context);
+    var language = AppLocalizations.of(context)!;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 45),
+      child: Scaffold(
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Text(
+            language.language,
+            style: theme.textTheme.bodyLarge,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          CustomDropdown<String>(
+            items: languages,
+            onChanged: (value) {},
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Text(
+            language.theme,
+            style: theme.textTheme.bodyLarge,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          CustomDropdown<String>(
+            items: themeLits,
+            onChanged: (value) {},
+          )
+        ]),
       ),
     );
   }
